5ce9c893a8d88fd6fbf66e7ac6563d5a3f67dcd6
Carlings <dzabrodskiy03@gmail.com>
2023-01-31 16:48:45 +0200

    Added a new entities: order and phone

---
+++ MarketPhone.WEB/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace MarketPhone.WEB.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Sum { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
+
+        public int PhoneId { get; set; }
+        public Phone Phone { get; set; }
+    }
+}
---
+++ MarketPhone.WEB/Models/Phone.cs
@@ -0,0 +1,12 @@
+﻿namespace MarketPhone.WEB.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public decimal Price { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
